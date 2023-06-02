953c4e53e035ad3d7da113dac591daeed114a024
tahseen001alam <alamtahseen33@gmail.com>
2023-06-03 00:02:24 +0530

    latest commit

--- assignment_3/Question_4.py
+++ assignment_3/Question_4.py
@@ -1,1 +1,18 @@
-print("Hello")
+def sii(nums, t):
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+
+        if nums[mid] == t:
+            return mid
+        elif nums[mid] < t:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return left
+nums=list(map(int,input().split()))
+t=int(input())
+print(sii(nums,t))
