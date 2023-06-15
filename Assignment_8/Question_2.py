b079954b44d42adbfa0c35cfbe24dce95fc17f86
tahseen001alam <alamtahseen33@gmail.com>
2023-06-15 23:42:16 +0530

    latest commit

--- Assignment_8/Question_2.py
+++ Assignment_8/Question_2.py
@@ -1,1 +1,29 @@
-print("H")
+def get_details(s):
+        st=[]
+        sr=[]
+        for i in range(len(s)):
+            if s[i]=='(':
+                st.append(i)
+            elif s[i]== '*':
+                sr.append(i)
+            else:
+                if st:
+                    st.pop()
+                elif sr:
+                    sr.pop()
+                else:
+                    return False
+        while st:
+            if len(sr)==0:
+                return False
+            elif st[-1]< sr[-1]:
+                st.pop()
+                sr.pop()
+            else:
+                return False
+        return True
+s="()()()(((*)))"
+print(get_details(s))
+
+
+
